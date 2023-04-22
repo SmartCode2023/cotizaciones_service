04fc1b29d2e95745aa4fa0f5f6b13544298b5604
Juan Sebastian Vargas Torres <libros123bmw@gmail.com>
2023-04-22 02:06:23 -0500

    ..

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./database/db");
-const cotizaciones = require("./routes/cotizaciones");
+import express, { json } from "express";
+import cors from "cors";
+import connectDB from "./database/db";
+import cotizaciones from "./routes/cotizaciones";
 
 const app = express();
 const port = 5100;
 
-app.use(express.json());
+app.use(json());
 app.use(cors());
-
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 
 
 app.use("/api/v1/cotizaciones", cotizaciones);
